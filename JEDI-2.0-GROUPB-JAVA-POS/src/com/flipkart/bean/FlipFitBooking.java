eac8b1c9f57f394fb988e15d3a7d8a6b4b6a2d82
sravyakaviti4303 <kavitisravya4303@gmail.com>
2024-07-17 11:59:16 +0530

    Gym Center

--- JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/bean/FlipFitBooking.java
+++ JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/bean/FlipFitBooking.java
@@ -1,46 +1,43 @@
 package com.flipkart.bean;
 
 public class FlipFitBooking {
-    private int userId;
+    private String bookingID;
+    private String userID;
+    private String scheduleID;
 
-    private int bookingId;
-
-    private int bookingId;
-
-    private int isAvailable;
-
-    private int slotId;
-
-    public int getBookingId() {
-        return bookingId;
+    public FlipFitBooking(String userID, String scheduleID) {
+        this.userID = userID;
+        this.scheduleID = scheduleID;
+    }
+    public FlipFitBooking(String bookingID,String userID, String scheduleID) {
+        this.bookingID = bookingID;
+        this.userID = userID;
+        this.scheduleID = scheduleID;
     }
 
-    public void setBookingId(int bookingId) {
-        this.bookingId = bookingId;
+
+    public String getBookingID() {
+        return bookingID;
     }
 
-    public int getUserId() {
-        return userId;
+    public void setBookingID(String bookingID) {
+        this.bookingID = bookingID;
     }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
+    public String getUserID() {
+        return userID;
     }
 
-    public int getIsAvailable() {
-        return isAvailable;
+    public void setUserID(String userID) {
+        this.userID = userID;
     }
 
-    public void setIsAvailable(int isAvailable) {
-        this.isAvailable = isAvailable;
+    public String getScheduleID() {
+        return scheduleID;
     }
 
-    public int getSlotId() {
-        return slotId;
-    }
-
-    public void setSlotId(int slotId) {
-        this.slotId = slotId;
+    public void setScheduleID(String scheduleID) {
+        this.scheduleID = scheduleID;
     }
 
 }
