0cbbe85d86548200fe9a039b86f1874a84300346
Nishtha <nishtha2242@gmail.com>
2024-07-17 12:08:22 +0530

    1st Commit

---
+++ JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/business/BookingService.java
@@ -0,0 +1,8 @@
+package com.flipkart.business;
+import java.util.Date;
+
+public interface BookingService {
+    boolean checkBookingOverlap(String customerId, Date date, String slotId);
+    void addBooking(String userId, String scheduleID);
+    void cancelBooking(String bookingID);
+}
