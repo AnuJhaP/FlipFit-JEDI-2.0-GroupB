b017b6311a5540bb5fac16767ace34a6a92081fb
MansiSoniDewal <mansi.soninlvm@gmail.com>
2024-07-18 12:04:41 +0530

    adminserviceimpl,gymownerdetailsadded

---
+++ JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/business/AdminServiceImpl.java
@@ -0,0 +1,28 @@
+package com.flipkart.business;
+
+import com.flipkart.bean.FlipFitCenter;
+import com.flipkart.bean.FlipFitGymOwner;
+
+import java.util.List;
+
+public class AdminServiceImpl implements AdminService {
+    @Override
+    public void validateCenter(int centerId, Boolean isApproved) {
+
+    }
+
+    @Override
+    public void validateOwner(int ownerId, Boolean isApproved) {
+
+    }
+
+    @Override
+    public List<FlipFitCenter> viewPendingCentres() {
+        return List.of();
+    }
+
+    @Override
+    public List<FlipFitGymOwner> viewPendingGymOwners() {
+        return List.of();
+    }
+}
