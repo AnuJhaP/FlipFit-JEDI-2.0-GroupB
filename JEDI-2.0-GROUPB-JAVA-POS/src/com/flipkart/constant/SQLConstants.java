9ca60bd2d4394372ef69098589dd17d1a2f3c889
raghav-arora3 <aroraraghav1820@gmail.com>
2024-07-19 14:50:34 +0530

    updated booking bean, sql const, added booking dao, schedule dao, userplan and exceptions

--- JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/constant/SQLConstants.java
+++ JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/constant/SQLConstants.java
@@ -14,5 +14,17 @@ public class SQLConstants {
     public static final String FETCH_ALL_PENDING_GYM_OWNERS_QUERY = "SELECT * FROM FlipFit.GymOwner where isApproved = 2";
     public static final String SEND_GYM_OWNER_APPROVAL_REQ_QUERY = "UPDATE FlipFit.GymOwner SET isApproved = 2 WHERE Id =?;";
     public static final String SQL_APPROVE_GYM_OWNER_BY_ID_QUERY="Update GymOwner Set isApproved=? WHERE Id=?";
+    public static final String ADD_SCHEDULE="INSERT INTO FlipFit.Schedule(scheduleId, date, slotId, availability ) values (?,?,?,?)";
+    public static final String GET_SCHEDULES_BY_DATE="SELECT * FROM FlipFit.Schedule WHERE date=?";
+    public static final String GET_SCHEDULE_BY_ID ="SELECT * FROM FlipFit.Schedule WHERE scheduleId=?";
+    public static final String MODIFY_SCHEDULE_AVAILABILITY ="UPDATE `flipfit`.`schedule` SET availability = ? WHERE (`scheduleId` = ?)";
+
+
+    public static final String ADD_BOOKING= "INSERT INTO FlipFit.Booking (bookingId, userID, scheduleID) values( ?, ?, ?)";
+    public static final String GET_BOOKING_BY_CUSTOMER_ID ="Select * From FlipFit.Booking where userID = ?";
+    public static final String CANCEL_BOOKING_BY_ID= "Delete from FlipFit.Booking where bookingId = ?";
+
+    public static final String GET_BOOKING_BY_BOOKING_ID ="Select * From FlipFit.Booking where bookingId = ?";
+    public static final String GET_USERPLAN_BY_CUSTOMER_ID = "select * from slot join schedule where slot.slotId=schedule.slotId and schedule.scheduleId=?";
 
 }
