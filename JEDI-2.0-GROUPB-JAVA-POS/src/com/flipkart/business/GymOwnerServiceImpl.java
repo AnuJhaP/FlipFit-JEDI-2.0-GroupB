c3802d7c92c7b3c3e679e0ae090b6a19b85be302
raghav-arora3 <aroraraghav1820@gmail.com>
2024-07-19 13:54:56 +0530

    adding gym owner dao

--- JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/business/GymOwnerServiceImpl.java
+++ JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/business/GymOwnerServiceImpl.java
@@ -1,28 +1,32 @@
 package com.flipkart.business;
 
+import com.flipkart.dao.GymOwnerDAO;
+
 import com.flipkart.bean.FlipFitGymOwner;
 
 import java.util.List;
 
 public class GymOwnerServiceImpl implements GymOwnerService {
-
+    private static GymOwnerDAO gymOwnerDAO = new GymOwnerDAO();
     @Override
     public void requestGymOwnerApproval(String gymOwnerId) {
-
+        gymOwnerDAO.sendOwnerApprovalRequest(gymOwnerId);
     }
 
     @Override
     public List<FlipFitGymOwner> viewAllGymOwners() {
-        return List.of();
+
+        return gymOwnerDAO.getGymOwnerList();
     }
 
     @Override
     public boolean loginGymOwner(String userId, String password) {
-        return false;
+
+        return gymOwnerDAO.loginGymOwner(userId,password);
     }
 
     @Override
     public void registerGymOwner(String userId, String userName, String password, String email, String panNumber, String cardNumber) {
-
+        gymOwnerDAO.registerGymOwner(new FlipFitGymOwner(userId,userName,email,password,panNumber,cardNumber));
     }
 }
---
+++ JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/dao/GymOwnerInterfaceDAO.java
@@ -0,0 +1,16 @@
+package com.flipkart.dao;
+
+import com.flipkart.bean.FlipFitGymOwner;
+
+import java.util.List;
+
+public interface GymOwnerInterfaceDAO {
+
+    public List<FlipFitGymOwner> getGymOwnerList();
+    public void setGymOwnerList(List<FlipFitGymOwner> gymOwnerList);
+    public void registerGymOwner(FlipFitGymOwner gymOwner);
+    public List<FlipFitGymOwner> getPendingGymOwnerList();
+    public void sendOwnerApprovalRequest(String gymOwnerId);
+    public void setPendingGymOwnerList();
+    public void validateGymOwner(String gymOwnerId, int isApproved);
+}
