ab7144bd3b3a2c73ca59bbe1ac7f3a69d602e42f
raghav-arora3 <aroraraghav1820@gmail.com>
2024-07-19 12:47:49 +0530

    updated bean of slot and payment

--- JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/bean/FlipFitPayment.java
+++ JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/bean/FlipFitPayment.java
@@ -1,34 +1,52 @@
 package com.flipkart.bean;
 
 public class FlipFitPayment {
-    private int userID;
-    // 1 -> UPI / 2 -> Debit Card
-    private int paymentType;
-    private String paymentInfo;
-    public String getPaymentInfo() {
-        return paymentInfo;
+//    private int userID;
+//    // 1 -> UPI / 2 -> Debit Card
+//    private int paymentType;
+//    private String paymentInfo;
+//    public String getPaymentInfo() {
+//        return paymentInfo;
+//    }
+//
+//    public void setPaymentInfo(String paymentInfo) {
+//        this.paymentInfo = paymentInfo;
+//    }
+//
+//    public int getPaymentType() {
+//        return paymentType;
+//    }
+//
+//    public void setPaymentType(int paymentType) {
+//        this.paymentType = paymentType;
+//    }
+//
+//
+//    public int getUserID() {
+//        return userID;
+//    }
+//
+//    public void setUserID(int userID) {
+//        this.userID = userID;
+//    }
+
+
+    private String paymentId;
+    private String amountPaid;
+    private String bookingId;
+
+    public String getPaymentId() { return paymentId; }
+
+    public void setPaymentId(String paymentId) { this.paymentId = paymentId; }
+
+    public String getAmountPaid() { return amountPaid; }
+
+    public void setAmountPaid(String amountPaid) {
+        this.amountPaid = amountPaid;
     }
 
-    public void setPaymentInfo(String paymentInfo) {
-        this.paymentInfo = paymentInfo;
-    }
+    public String getBookingId() { return bookingId; }
 
-    public int getPaymentType() {
-        return paymentType;
-    }
-
-    public void setPaymentType(int paymentType) {
-        this.paymentType = paymentType;
-    }
-
-
-    public int getUserID() {
-        return userID;
-    }
-
-    public void setUserID(int userID) {
-        this.userID = userID;
-    }
-
+    public void setBookingId(String bookingId) { this.bookingId = bookingId;}
 
 }
