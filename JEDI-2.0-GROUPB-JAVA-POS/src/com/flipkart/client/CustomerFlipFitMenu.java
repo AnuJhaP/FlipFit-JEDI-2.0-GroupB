75d1f5d58deb256e29b3ce62bd1a6d1c1476e357
raghav-arora3 <aroraraghav1820@gmail.com>
2024-07-20 17:16:22 +0530

    updated menus in client pkg, resolved errors

--- JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/client/CustomerFlipFitMenu.java
+++ JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/client/CustomerFlipFitMenu.java
@@ -1,150 +1,3 @@
-//package com.flipkart.client;
-//
-//import com.flipkart.bean.Address;
-//import com.flipkart.bean.FlipFitBooking;
-//import com.flipkart.bean.FlipFitCustomer;
-//import com.flipkart.business.CustomerService;
-//import com.flipkart.business.CustomerServiceImpl;
-//import com.flipkart.utils.FlipFitUtils;
-//
-//import static com.flipkart.client.FlipFitApplicationClient.scanner;
-//import static com.flipkart.constant.FlipFitConstant.*;
-//
-//import java.time.LocalDateTime;
-//import java.time.format.DateTimeFormatter;
-//import java.util.Date;
-//import java.util.List;
-//import java.util.Scanner;
-//
-//public class CustomerFlipFitMenu {
-//
-//    private CustomerService customerService = new CustomerServiceImpl();
-//    private FlipFitUtils utils = new FlipFitUtils();
-//    public void customerRegister(){
-//
-//
-//        System.out.println("Registering Customer.........");
-//        System.out.println("Enter your User Name");
-//        Scanner scanner = new Scanner(System.in);
-//        String userName = scanner.nextLine();
-//
-//
-//        System.out.println("Enter your email address.");
-//        String email = scanner.nextLine();
-//
-//        System.out.println("Enter your password.");
-//        String password = scanner.nextLine();
-//
-//        System.out.println("Enter your password again.");
-//        String passwordAgain = scanner.nextLine();
-//
-//        System.out.println("Enter your phone number.");
-//        String phoneNumber = scanner.nextLine();
-//
-//        System.out.println("Enter your age.");
-//        int age = scanner.nextInt();
-//
-//        System.out.println("Enter your Address - City:");
-//        String city = scanner.next();
-//        System.out.println("Enter your Address - State:");
-//        String state = scanner.next();
-//        System.out.println("Enter your Address - Pin Code:");
-//        String pinCode = scanner.next();
-//        System.out.println("Enter your Address - Street Address:");
-//        String streetAddress = scanner.next();
-//
-//        Address address = new Address(city, state, pinCode, streetAddress);
-//
-//        System.out.println("Customer Registered Successfully.");
-//
-//        customerService.registerCustomer(userName,password,email,phoneNumber);
-//        customerClientMainPage(userName);
-//    }
-//
-//    public void customerClientMainPage(String userName) {
-//        LocalDateTime currentTime = LocalDateTime.now();
-//        DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-//        String formattedDate = currentTime.format(myFormat);
-//        System.out.println("WELCOME "+userName+" !!\nWhat you what to do\nLogin TIME: "+currentTime);
-//        while(true) {
-//            System.out.println("1. View My Profile \n2. Book a slot in a Gym \n3. View Bookings\n4. Cancel Bookings\n5. Go Back to previous menu");
-//            int choice = scanner.nextInt();
-//            switch (choice) {
-//                case 1:
-//                    FlipFitCustomer customer = customerService.viewMyProfile(userName);
-//                    utils.printCustomerProfile(customer);
-//                    break;
-//                case 2:
-//                    bookSlotSubMenu(userName);
-//                    break;
-//                case 3:
-//                    printUserPlan(userName);
-//                    break;
-//                case 4:
-//                    cancelBookingSubMenu(userName);
-//                    break;
-//                case 5:
-//                    System.out.println("\nGOING BACK TO PREVIOUS MENU\n");
-//                    return;
-//                default:
-//                    System.out.println("\nPlease enter valid choice\n");
-//                    break;
-//            }
-//        }
-//    }
-//
-//
-//    private void cancelBookingSubMenu(String userId){
-//        System.out.println("Select the Booking you want to cancel: ");
-//        printbookingsSubMenu(userId);
-//        String bookingId = scanner.next();
-//        customerService.cancelBookingbyID(bookingId);
-//
-//    }
-//
-//    private void printbookingsSubMenu(String userName){
-//        System.out.println("Bookings : ");
-//        List<FlipFitBooking> allBookingList= customerService.getCustomerBookings(userName);
-//        System.out.println(DASHED_LINE);
-//        System.out.printf(YELLOW_COLOR + "%-8s\t", "BOOKING-ID");
-//        System.out.printf("%47s\t\n", "SCHEDULE-ID" + RESET_COLOR);
-//        System.out.println(DASHED_LINE);
-//        for(FlipFitBooking booking: allBookingList) {
-//            System.out.printf("%-8s\t", booking.getBookingId());
-//           // System.out.printf("%-8s\t\n", booking.getScheduleID());
-//        }
-//        System.out.println(DASHED_LINE);
-//    }
-//
-//    private void bookSlotSubMenu(String userId){
-//        System.out.println("Book Slot");
-////        String location = scanner.next();
-////        List<GymCentre> centreListByLocation = customerService.getAllGymCenterDetailsByCity(location);
-////        // Print All Centres
-////        utils.printGymCentres(centreListByLocation);
-////        //Select Gym Centre
-////        if(centreListByLocation.isEmpty()){
-////            System.out.println(RED_COLOR +"There are no available GYM Centres in " + location + ". Please Select some other location" + RESET_COLOR);
-////            bookSlotSubMenu(userName);
-////            return;
-////        }
-////        System.out.print("Choose a gymCentre ID to proceed:");
-////        String chosenGym = scanner.next();
-////        //Select Date
-////        Date sqlDate = selectDate();
-////        //Choose Slot
-////        chooseSlot(chosenGym,userName,sqlDate,chosenGym);
-//    }
-//
-//    private void printUserPlan(String userId) {
-//        System.out.println("Print User Plan");
-//
-//    }
-//
-//}
-
-
-
 package com.flipkart.client;
 
 import com.flipkart.bean.FlipFitBooking;
@@ -165,13 +18,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import static com.flipkart.client.FlipFitApplicationClient..scanner;
+import static com.flipkart.client.FlipFitApplicationClient.scanner;
 import static com.flipkart.constant.FlipFitConstant.*;
 import static com.flipkart.constant.FlipFitConstant.RESET_COLOR;
 
 
-public class CustomerClient {
-    private CustomerService customerService  =  new CustomerService();
+public class CustomerFlipFitMenu {
+    private CustomerServiceImpl customerService  =  new CustomerServiceImpl();
 
     public boolean customerLogin(String userName, String password) {
 //        Check if credentials are right
--- JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/client/FlipFitApplicationClient.java
+++ JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/client/FlipFitApplicationClient.java
@@ -1,102 +1,99 @@
 package com.flipkart.client;
 
 import com.flipkart.bean.Role;
-
 import java.util.Random;
 import java.util.Scanner;
 
 import static com.flipkart.bean.Role.*;
-
+import static com.flipkart.constant.FlipFitConstant.*;
+import com.flipkart.constant.FlipFitConstant;
 public class FlipFitApplicationClient {
+
 
     public static int userId = 0;
     public static Scanner scanner = new Scanner(System.in);
-    private static CustomerFlipFitMenu customerFlipFitMenu = new CustomerFlipFitMenu();
-    private static GymOwnerFlipFitMenu gymOwnerFlipFitMenu = new GymOwnerFlipFitMenu();
-    private static AdminFlipFitMenu flipFitAdminMenu = new AdminFlipFitMenu();
+    private static AdminFlipFitMenu adminClient = new AdminFlipFitMenu();
+    private static CustomerFlipFitMenu customerClient = new CustomerFlipFitMenu();
+    private static GymOwnerFlipFitMenu gymOwnerClient = new GymOwnerFlipFitMenu();
 
 
     private static void mainPage(){
-        System.out.println("1. Login\n2. Register\n3. Change Password\n4. Exit");
+        System.out.println("1. Login\n2. Registration\n3. Exit");
         int choice = scanner.nextInt();
         switch (choice) {
             case 1:
                 login();
                 break;
             case 2:
-                register();
+                registration();
                 break;
             case 3:
-                changePassword();
-                break;
-            case 4:
-                System.out.println("exit");
+                System.out.println(EXIT_MESSAGE);
                 return;
             default:
-                System.out.println("invalid_choice_error");
+                System.out.println(INVALID_CHOICE_ERROR);
                 break;
         }
         mainPage();
     }
 
-    private static void register(){
-
-        System.out.println("Enter your role");
-//        Role role=Role.valueOf(scanner.next());
-        Role role = new Role("abs",scanner.next());
-
-        switch (role.getDescription()){
-            case "ADMIN":
-                System.out.println("Admin is already registered");
-                flipFitAdminMenu.AdminMenu();
-                break;
-            case "GYMOWNER":
-                gymOwnerFlipFitMenu.gymOwnerRegister();
-                break;
-            case "CUSTOMER":
-                customerFlipFitMenu.customerRegister();
-                break;
-            default:
-                System.out.println("invalid_choice");
-                break;
-        }
-    }
-
     private static void login(){
-        System.out.println("Enter your User Name");
-        String userName = scanner.next();
+        try {
+            System.out.println("Enter your Role");
+            Role role = Role.valueOf(scanner.next().toUpperCase());
 
-        System.out.println("Enter your Password");
-        String password = scanner.next();
+            System.out.println("Enter your UserName");
+            String userName = scanner.next();
 
-        System.out.println("Enter your role");
-        Role role = new Role("abs",scanner.next());
+            System.out.println("Enter your Passkey");
+            String password = scanner.next();
 
-        switch (role.getDescription()){
-            case "ADMIN":
-                flipFitAdminMenu.AdminMenu();
-                break;
-            case "GYMOWNER":
-                gymOwnerFlipFitMenu.gymOwnerClientMainPage(userName);
-                break;
-            case "CUSTOMER":
-                customerFlipFitMenu.customerClientMainPage(userName);
-                break;
-            default:
-                System.out.println("invalid_choice");
-                break;
+            switch (role){
+                case ADMIN:
+                    adminClient.adminLogin(userName,password);
+                    break;
+                case GYMOWNER:
+                    gymOwnerClient.gymOwnerLogin(userName,password);
+                    break;
+                case CUSTOMER:
+                    customerClient.customerLogin(userName,password);
+                    break;
+                default:
+                    System.out.println(INVALID_CHOICE_ERROR);
+                    break;
+            }
+        }catch (IllegalArgumentException e){
+            System.out.println(INVALID_CHOICE_ERROR);
         }
-
-        System.out.println("You have been successfully logged in");
-
     }
 
-    private static void changePassword(){
+    private static void registration(){
+        try {
+            System.out.println("Enter your role");
+            Role role = Role.valueOf(scanner.next().toUpperCase());
 
+            switch (role){
+                case ADMIN:
+                    System.out.println("Admin is already registered");
+                    mainPage();
+                    break;
+                case CUSTOMER:
+                    customerClient.register();
+                    break;
+                case GYMOWNER:
+                    gymOwnerClient.register();
+                    break;
+                default:
+                    System.out.println(INVALID_CHOICE_ERROR);
+                    break;
+            }
+        }catch (IllegalArgumentException e){
+            System.out.println(INVALID_CHOICE_ERROR);
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome TO FlipFit");
+        System.out.println(WELCOME_MESSAGE);
         mainPage();
     }
 }
