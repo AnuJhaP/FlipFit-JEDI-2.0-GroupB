c6b75d77a9b6dd4f3b85338abfbc9987ed19d820
sravyakaviti4303 <kavitisravya4303@gmail.com>
2024-07-22 15:18:26 +0530

    changes added

--- JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/exceptions/DataEntryException.java
+++ JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/exceptions/DataEntryException.java
@@ -1,10 +1,22 @@
 package com.flipkart.exceptions;
+// Importing static constants from FlipFitConstant for coloring the output
 
 import static com.flipkart.constant.FlipFitConstant.RED_COLOR;
 import static com.flipkart.constant.FlipFitConstant.YELLOW_COLOR;
 
+/**
+ * Custom exception class to handle data entry errors.
+ * This class extends RuntimeException and provides a custom error message.
+ */
+
 public class DataEntryException extends RuntimeException{
+    /**
+     * Constructor for DataEntryException.
+     * It provides a default error message indicating an incorrect data format.
+     */
     public DataEntryException(){
+        // Calling the constructor of the superclass (RuntimeException) with a colored message
+        // The message is in red to indicate an error and ends with yellow to highlight the need for format reference
         super(RED_COLOR+"Incorrect Data format. Please refer to the format."+YELLOW_COLOR);
     }
 }
