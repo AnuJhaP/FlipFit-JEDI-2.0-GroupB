ede9d32cde9881c9bb23e2309d98ee15e3fbbb66
Pooja Yadav <poojayadav100418@gmail.com>
2024-07-22 11:06:34 +0530

    Addede

--- JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/constant/SQLConstants.java
+++ JEDI-2.0-GROUPB-JAVA-POS/src/com/flipkart/constant/SQLConstants.java
@@ -11,16 +11,19 @@ public class SQLConstants {
     /**
      * Slot SQL Queries
      */
+    public static final String ADD_GYM_CENTRE_QUERY = "INSERT INTO  flipfit-schema.FlipFitGymCentre (centreId, ownerId, centreName, gstin, city, capacity, price, isApproved) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
     public static final String FETCH_ALL_SLOTS ="SELECT * FROM flipfit-schema.FlipFitSlot";
     public static final String FETCH_SLOT_BY_CENTRE ="SELECT * FROM flipfit-schema.FlipFitSlot WHERE centreId=?";
     public static final String ADD_SLOT ="INSERT INTO flipfit-schema.FlipFitSlot(slotId, centreId, time) values (?, ?, ?)";
     public static final String FETCH_SLOT_BY_ID = "SELECT * FROM flipfit-schema.FlipFitSlot WHERE slotId=?";
     public static final String FETCH_SLOT_BY_ID_AND_CENTRE_ID = "SELECT * FROM flipfit-schema.FlipFitSlot WHERE slotId=? AND centreId=?";
-
-
+    public static final String SQL_APPROVE_GYM_CENTRE_BY_ID_QUERY="Update FlipFitGymCentre Set isApproved=? WHERE centreId=?";
+    public static final String FETCH_GYM_CENTRES_BY_CITY = "SELECT * FROM flipfit-schema.FlipFitGymCentre where city = ?";
+    public static final String FETCH_ALL_PENDING_GYM_CENTRES_QUERY = "SELECT * FROM flipfit-schema.FlipFitGymCentre  where isApproved = 2";
     public static final String FETCH_ALL_GYM_OWNERS_QUERY = "SELECT * FROM flipfit-schema.FlipFitGymOwner";
     public static final String LOGIN_GYM_OWNER = "Select * from FlipFitGymOwner where name=? and password=?";
     public static final String REGISTER_GYM_OWNER = "Insert into FlipFitGymOwner values (?,?,?,?,?,?,?)";
+    public static final String FETCH_GYM_CENTRES_BY_OWNER_ID = "SELECT * FROM flipfit-schema.FlipFitGymCenter where ownerId = ?";
     public static final String FETCH_ALL_PENDING_GYM_OWNERS_QUERY = "SELECT * FROM flipfit-schema.FlipFitGymOwner where isApproved = 2";
     public static final String SEND_GYM_OWNER_APPROVAL_REQ_QUERY = "UPDATE flipfit-schema.FlipFitGymOwner SET isApproved = 2 WHERE Id =?;";
     public static final String SQL_APPROVE_GYM_OWNER_BY_ID_QUERY="Update FlipFitGymOwner Set isApproved=? WHERE Id=?";
@@ -29,7 +32,7 @@ public class SQLConstants {
     public static final String GET_SCHEDULE_BY_ID ="SELECT * FROM flipfit-schema.FlipFitSchedule WHERE scheduleId=?";
     public static final String MODIFY_SCHEDULE_AVAILABILITY ="UPDATE `flipfit`.`schedule` SET availability = ? WHERE (`scheduleId` = ?)";
 
-
+    public static final String FETCH_GYM_CENTRE_BY_ID = "SELECT * FROM flipfit-schema.FlipFitGymCentre where centreId = ?";
     public static final String ADD_BOOKING= "INSERT INTO flipfit-schema.FlipFitBooking (bookingId, userID, scheduleID) values( ?, ?, ?)";
     public static final String GET_BOOKING_BY_CUSTOMER_ID ="Select * From flipfit-schema.FlipFitBooking where userID = ?";
     public static final String CANCEL_BOOKING_BY_ID= "Delete from flipfit-schema.FlipFitBooking where bookingId = ?";
